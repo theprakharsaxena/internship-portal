3f78fd6944151e130ad239ee5fb5140ba40b77e8
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-08-27 20:58:13 +0530

    pushed

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -4,8 +4,4 @@ const AxiosClient = axios.create();
 
 AxiosClient.defaults.baseURL = process.env.REACT_APP_BASE_URL || "https://internship-43hq.onrender.com";
 
-AxiosClient.defaults.headers = {
-  "Content-Type": "application/json",
-};
-
 export default AxiosClient;

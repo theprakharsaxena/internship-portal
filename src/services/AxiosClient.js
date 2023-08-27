6843ea5319c06d5091397858856ac3264c90e7a8
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-08-27 20:51:38 +0530

    new change

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const AxiosClient = axios.create();
 
-console.log("+++++++++++", process.env.REACT_APP_BASE_URL);
-
 AxiosClient.defaults.baseURL = process.env.REACT_APP_BASE_URL || "https://internship-43hq.onrender.com";
 
 AxiosClient.defaults.headers = {
